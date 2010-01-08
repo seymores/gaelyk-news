abf93826d7d5b28e45394dca012a1a655de311b1
Seymour Cakess <seymores@gmail.com>
2010-01-08 11:19:10 +0800

    Added pretty time lib

--- war/WEB-INF/groovy/datetime.groovy
+++ war/WEB-INF/groovy/datetime.groovy
@@ -1,3 +1,10 @@
-request.setAttribute 'datetime', new Date().toString()
+import com.ocpsoft.pretty.time.PrettyTime
+
+java.util.Date.metaClass.prettyTime = {->
+    def p = new PrettyTime()
+    return p.format( delegate )
+}
+
+request.setAttribute 'datetime', new Date().prettyTime()
 
 forward '/datetime.gtpl'
